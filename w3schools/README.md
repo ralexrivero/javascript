ce7b8cfb42c08fe5d2b7d6f9660d6cfa31f75716
ralexrivero <ralexrivero@gmail.com>
2022-07-23 02:03:45 -0300

    send output

--- w3schools/README.md
+++ w3schools/README.md
@@ -7,3 +7,13 @@
 - `document.getElementById("demo").innerHTML = "Hello JavaScript!";` to change the content of an element
 
 - `getElementsByClassName` returns an array of objects
+
+### Display
+
+- `innerHTML` html content
+- `document.write()` for testing purposes in inner `<script>`
+- `window.alert()`
+- `console.log()` for debugging purposes
+- `window.print()` print the current page
+
+###
---
+++ w3schools/intro/01-output.js
@@ -0,0 +1,15 @@
+const mainTitle = document.getElementById('mainTitle')
+mainTitle.innerHTML = `<h1>Main title from JavaScript</h1>`
+
+const btnAlert = document.getElementById('btnAlert')
+btnAlert.addEventListener('click', function() {
+    window.alert('This an alert!');
+});
+
+console.log('Here!\nThis is a debugging message');
+
+
+const printPage = document.getElementById('printPage')
+printPage.addEventListener('click', function() {
+    window.print();
+});
