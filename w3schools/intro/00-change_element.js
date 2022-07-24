a8428ec35d2e614ae130ed22d83961ffa02cf49d
ralexrivero <ralexrivero@gmail.com>
2022-07-23 21:52:50 -0300

    semistandard style

--- w3schools/intro/00-change_element.js
+++ w3schools/intro/00-change_element.js
@@ -1,40 +1,40 @@
-document.getElementById("header").innerHTML = "Hello JavaScript!";
-const paragraph = document.getElementsByClassName("paragraph");
+document.getElementById('header').innerHTML = 'Hello JavaScript!';
+const paragraph = document.getElementsByClassName('paragraph');
 
 for (let i = 0; i < paragraph.length; i++) {
-    paragraph[i].style.fontSize = "12px";
-};
+  paragraph[i].style.fontSize = '12px';
+}
 
-const turnOn = document.getElementById("turn-on")
-const turnOff = document.getElementById("turn-off")
+const turnOn = document.getElementById('turn-on');
+const turnOff = document.getElementById('turn-off');
 
-turnOn.addEventListener("click", function() {
-    document.getElementById("change-image").src="./pic_bulbon.gif";
+turnOn.addEventListener('click', function () {
+  document.getElementById('change-image').src = './pic_bulbon.gif';
 });
 
-turnOff.addEventListener("click", function() {
-    document.getElementById("change-image").src="./pic_bulboff.gif"
+turnOff.addEventListener('click', function () {
+  document.getElementById('change-image').src = './pic_bulboff.gif';
 });
 
-function funSwitchPar() {
-    const par = document.getElementById("paragraph_display");
-    if (par.style.display === 'block') {
-        par.style.display='none';
-    } else {
-        par.style.display='block';
-    }
-};
-const btnSwitch = document.getElementById("btn_display");
-btnSwitch.addEventListener("click", funSwitchPar);
+function funSwitchPar () {
+  const par = document.getElementById('paragraph_display');
+  if (par.style.display === 'block') {
+    par.style.display = 'none';
+  } else {
+    par.style.display = 'block';
+  }
+}
+const btnSwitch = document.getElementById('btn_display');
+btnSwitch.addEventListener('click', funSwitchPar);
 
-function funChangeText() {
-    document.getElementById('change_paragraph').innerHTML = "Paragraph has been changed successfully";
+function funChangeText () {
+  document.getElementById('change_paragraph').innerHTML = 'Paragraph has been changed successfully';
 }
 
 let count = 0;
 
-const click_count = document.getElementById('btn_counter');
-click_count.addEventListener('click', function() {
-    count += 1;
-    document.getElementById('counter').innerHTML = `Total: ${count}`;
+const clickCount = document.getElementById('btn_counter');
+clickCount.addEventListener('click', function () {
+  count += 1;
+  document.getElementById('counter').innerHTML = `Total: ${count}`;
 });
