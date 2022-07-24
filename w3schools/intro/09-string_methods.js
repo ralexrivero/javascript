de20c1c861743dc9ee62965986e616c32c65c13d
ralexrivero <ralexrivero@gmail.com>
2022-07-24 18:40:10 -0300

    string methods

--- w3schools/intro/09-string_methods.js
+++ w3schools/intro/09-string_methods.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 /*
   string methods
+  strings are immutable, string methods always returns a new string
 */
 
 const str1 = 'Hello World and good bye World';
@@ -10,3 +11,11 @@ console.log(str2);
 console.log(str3);
 const str4 = str1.replace(/world/ig, 'JS'); // case insensitive and all matches
 console.log(str4);
+
+const a = 'Ronald';
+const b = 'Alexander';
+const c = 'Rivero';
+const d = 'Espindola';
+const e = a.concat(' ', b, ' ', c, ' ', d);
+console.log(e);
+console.log(a);
