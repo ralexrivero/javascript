995e1bfc577ec661d32297efb50537c321c226a3
ralexrivero <ralexrivero@gmail.com>
2022-07-23 21:53:07 -0300

    bitwise operator

---
+++ w3schools/intro/03-logical_operators.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/*
+logical operators && (AND), || (OR) and ! (NOT)
+*/
+
+const a = true;
+const b = false;
+
+console.log(a && b); // false
+console.log(a || b); // true
+console.log(!a); // false
---
+++ w3schools/intro/04-bitwise_operator.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/*
+bitwise operators
+ - & AND
+ - | OR
+ - ~ NOT
+ - ^ XOR
+ - << LEFT SHIFT
+ - >> RIGHT SHIFT
+ - >>> UNSIGNED RIGHT SHIFT
+*/
+
+let a = 5, b = 5;
+
+let c = a & b;
+console.log(c);
