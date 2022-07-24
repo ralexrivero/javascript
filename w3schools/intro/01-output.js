ffbfd2a6ba4e41a09c8f9674e914cd678f8831e6
ralexrivero <ralexrivero@gmail.com>
2022-07-23 21:57:13 -0300

    semistandard style

--- w3schools/intro/01-output.js
+++ w3schools/intro/01-output.js
@@ -1,15 +1,14 @@
-const mainTitle = document.getElementById('mainTitle')
-mainTitle.innerHTML = `<h1>Main title from JavaScript</h1>`
+const mainTitle = document.getElementById('mainTitle');
+mainTitle.innerHTML = '<h1>Main title from JavaScript</h1>';
 
-const btnAlert = document.getElementById('btnAlert')
-btnAlert.addEventListener('click', function() {
-    window.alert('This an alert!');
+const btnAlert = document.getElementById('btnAlert');
+btnAlert.addEventListener('click', function () {
+  window.alert('This an alert!');
 });
 
 console.log('Here!\nThis is a debugging message');
 
-
-const printPage = document.getElementById('printPage')
-printPage.addEventListener('click', function() {
-    window.print();
+const printPage = document.getElementById('printPage');
+printPage.addEventListener('click', function () {
+  window.print();
 });
