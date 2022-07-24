0439f7ab73a9290154f1ac4d74fe72675d1997b4
ralexrivero <ralexrivero@gmail.com>
2022-07-24 00:31:09 -0300

    typeof unary operator

---
+++ w3schools/intro/06-typeof.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/*
+  typeof operator
+*/
+
+console.log(typeof (''));
+console.log(typeof (5));
+const myObject = { name: 'Ronald', surename: 'Rivero' };
+console.log(typeof (myObject));
+console.log(typeof (5 > 6));
+const myArr = ['a', 'b'];
+console.log(typeof (myArr));
