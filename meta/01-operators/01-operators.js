d094791935d0c9442c0b94290d373f281a87f816
ralexrivero <ralexrivero@gmail.com>
2022-12-14 20:59:37 -0300

    add operators

---
+++ meta/01-operators/01-operators.js
@@ -0,0 +1,108 @@
+#!/usr/bin/env node
+
+/* Arithmetic operators
+  * multiplication
+  + addition
+  - subtraction
+  * multiplication
+  / division
+  % modulus
+  ++ increment
+  -- decrement
+*/
+
+/* Assignment operators
+  = assignment
+  += addition assignment
+  -= subtraction assignment
+  *= multiplication assignment
+  /= division assignment
+  %= modulus assignment
+*/
+
+/* Comparison operators
+  == equal to
+  === equal value and equal type
+  != not equal
+  !== not equal value or not equal type
+  > greater than
+  < less than
+  >= greater than or equal to
+  <= less than or equal to
+  ? ternary operator
+*/
+
+/* Logical operators
+  && logical and
+  || logical or
+  ! logical not
+*/
+
+/* Bitwise operators
+  & bitwise and
+  | bitwise or
+  ^ bitwise xor
+  ~ bitwise not
+  << left shift
+  >> right shift
+  >>> zero fill right shift
+*/
+
+/* String operators
+  + addition
+  += addition assignment
+*/
+
+/* Conditional (ternary) operator
+  condition ? expr1 : expr2
+*/
+
+/* Comma operator
+  expr1, expr2
+*/
+
+/* Unary operators
+  delete deletes an object's property
+  typeof returns a string indicating the type of the unevaluated operand
+  void discards an expression's return value
+*/
+
+/* Relational operators
+  in returns true if the specified property is in the specified object or its prototype chain
+  instanceof returns true if the specified object is of the specified object type
+*/
+
+/* Equality operators
+  == equal to
+  === equal value and equal type
+  != not equal
+  !== not equal value or not equal type
+*/
+
+let a = 1;
+const b = 2;
+const c = 3;
+const d = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a);
+console.log(a--);
+console.log(a);
+console.log(--a);
+console.log(a);
+
+console.log(a += b);
+console.log(a -= b);
+console.log(a *= b);
+console.log(a /= b);
+console.log(a %= b);
+
+console.log(a > b);
+console.log(c === d);
