af075c31b2488d665ffd42c2f04c9b678793561a
ralexrivero <ralexrivero@gmail.com>
2022-12-14 21:46:26 -0300

    strings literals

---
+++ meta/02-data_types/02-strings.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/* String delimiters
+    'single quotes'
+    "double quotes"
+    `backticks`
+*/
+
+const greet = 'Hello';
+const firstName = 'Rony';
+
+console.log(greet + ' ' + firstName + '!');
+console.log(`${greet} ${firstName}!`);
