9b12199abade1f8247292cd5eebec395673f4eb2
ralexrivero <ralexrivero@gmail.com>
2022-12-15 00:34:42 -0300

    statements

---
+++ meta/03-statements/03-if_statements.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+/*
+  If statements are used to execute code if a condition is true.
+*/
+
+let result = 50;
+if (result > 45) {
+  console.log('You passed the test!');
+} else {
+  console.log("You failed the test! Don't worry, you can try again.");
+}
+
+let food = 'hot';
+
+if (food === 'hot') {
+  console.log('The food is hot!');
+} else if (foot === 'cold') {
+  console.log('The food is cold!');
+} else {
+  console.log('The food is neither hot nor cold!');
+}
