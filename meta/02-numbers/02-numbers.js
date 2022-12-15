151f9160a0a078e39fb91b809d159f7ccbf6c3f4
ralexrivero <ralexrivero@gmail.com>
2022-12-14 21:41:23 -0300

    numbers

---
+++ meta/02-numbers/02-numbers.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const a = 1;
+const b = 2.3;
+const c = 3e94;
+
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (a + b));
+console.log(typeof (c));
+console.log(c);
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.NaN);
