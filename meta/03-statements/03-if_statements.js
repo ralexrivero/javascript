263e94c8a9deeb798e816f8be65affef30d924c3
ralexrivero <ralexrivero@gmail.com>
2022-12-15 14:34:33 -0300

    fix syntax error, style and add if else statement

--- meta/03-statements/03-if_statements.js
+++ meta/03-statements/03-if_statements.js
@@ -4,19 +4,34 @@
   If statements are used to execute code if a condition is true.
 */
 
-let result = 50;
+const result = 50;
 if (result > 45) {
   console.log('You passed the test!');
 } else {
   console.log("You failed the test! Don't worry, you can try again.");
 }
 
-let food = 'hot';
+const food = 'hot';
 
 if (food === 'hot') {
   console.log('The food is hot!');
-} else if (foot === 'cold') {
+} else if (food === 'cold') {
   console.log('The food is cold!');
 } else {
   console.log('The food is neither hot nor cold!');
 }
+
+const age = 65;
+let message = '';
+
+if (age >= 65) {
+  message = 'You get your income from your pension';
+} else if (age >= 18) {
+  message = 'Each month you get a salary';
+} else if (age < 18) {
+  message = 'You get an allowance';
+} else {
+  message = 'The value of your age is not numerical';
+}
+
+console.log(message);
--- meta/03-statements/03-switch_statement.js
+++ meta/03-statements/03-switch_statement.js
@@ -9,16 +9,16 @@
 
 const place = 'first';
 
-switch(place) {
-    case 'first':
-        console.log('Gold');
-        break;
-    case 'second':
-        console.log('Silver');
-        break;
-    case 'third':
-        console.log('Bronze');
-        break;
-    default:
-        console.log('No medal awarded');
+switch (place) {
+  case 'first':
+    console.log('Gold');
+    break;
+  case 'second':
+    console.log('Silver');
+    break;
+  case 'third':
+    console.log('Bronze');
+    break;
+  default:
+    console.log('No medal awarded');
 }
