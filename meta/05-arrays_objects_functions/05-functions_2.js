a696fadfe95e28a3bbf606aafe2a22e0281a6ea1
ralexrivero <ralexrivero@gmail.com>
2022-12-16 01:57:05 -0300

    add functions with array parameter

--- meta/05-arrays_objects_functions/05-functions_2.js
+++ meta/05-arrays_objects_functions/05-functions_2.js
@@ -12,3 +12,13 @@ function arrAdd (arr) {
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(arrAdd(myNums));
+
+function printArr (arr = []) {
+  for (let x = 0; x < arr.length; x++) {
+    console.log(`${x}: ${arr[x]}`);
+  }
+}
+
+const myLanguages = ['Python', 'C', 'JavaScript', 'TypeScript', 'SQL', 'VBA'];
+
+printArr(myLanguages);
