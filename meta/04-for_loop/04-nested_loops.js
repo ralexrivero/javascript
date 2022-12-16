4cf6bbca3d52439d682fa7cc58a0ff2a4c0eefc7
ralexrivero <ralexrivero@gmail.com>
2022-12-16 01:19:51 -0300

    nested loops

---
+++ meta/04-for_loop/04-nested_loops.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+/* nested loops
+  nested loops are loops inside loops
+  the syntax of nested loops is:
+
+  for (initialization; condition; increment/decrement) {
+    for (initialization; condition; increment/decrement) {
+      // code block to be executed
+    };
+    };
+*/
+
+const year = [2022, 2023];
+const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+for (const x in year) {
+  for (const y in month) {
+    console.log(`${year[x]} - ${month[y]}`)
+  }
+};
