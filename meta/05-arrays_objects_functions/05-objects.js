4ae546ad9e735bc3c3333b264201a4d2df2d0dde
ralexrivero <ralexrivero@gmail.com>
2022-12-16 02:38:15 -0300

    add function, inheritance and prototype

--- meta/05-arrays_objects_functions/05-objects.js
+++ meta/05-arrays_objects_functions/05-objects.js
@@ -5,7 +5,7 @@ const assistantManager = {
   socialSkills: 30,
   streetSmarts: 30,
   helth: 40
-}
+};
 
 /* dot operator
    is used to access object properties
@@ -15,3 +15,29 @@ const assistantManager = {
 assistantManager.seniority = 5;
 
 console.log(assistantManager);
+
+function Animal () {
+  this.eat = function () {
+    console.log('The animal is eating');
+  };
+}
+
+function Mammal () {
+  this.walk = function () {
+    console.log('The mammal is walking');
+  };
+}
+
+Mammal.prototype = new Animal();
+
+const dog = new Mammal();
+
+dog.walk();
+dog.eat();
+
+dog.name = 'Snooky';
+dog.age = 7;
+dog.weight = 12;
+dog.color = 'brown';
+
+console.log(dog);
