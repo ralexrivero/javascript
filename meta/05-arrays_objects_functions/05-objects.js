e9bf30a85f5fc49ea9efc948580ac61e088544e2
ralexrivero <ralexrivero@gmail.com>
2022-12-16 02:16:02 -0300

    add objects using key-value pairs and dot notation

---
+++ meta/05-arrays_objects_functions/05-objects.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const assistantManager = {
+  movement: 3,
+  socialSkills: 30,
+  streetSmarts: 30,
+  helth: 40
+}
+
+/* dot operator
+   is used to access object properties
+  and used to access object methods
+*/
+
+assistantManager.seniority = 5;
+
+console.log(assistantManager);
