f6645cd55ea9dd0d2ecbba77e4a8fbdbda8736d2
ralexrivero <ralexrivero@gmail.com>
2022-12-19 19:08:42 -0300

    try throw

--- meta/08-error_handling/08-try_catch.js
+++ meta/08-error_handling/08-try_catch.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+// let a = 1;
+// let b = 2;
 
 try {
-    console.log(a + b);
-} catch(err) {
-    consol.log(err);
-    console.log('The error was saved y the error log');
+  console.log(a + b);
+} catch (err) {
+  console.log(err)
+  console.log('There was an error');
+  console.log('The error was saved in the error log');
 }
 
-console.log("m program does not stop");
+console.log('The program does not stop');
---
+++ meta/08-error_handling/08-try_throw.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+try {
+  throw new ReferenceError();
+} catch (err) {
+  console.log(err);
+  console.log('There was a Reference Error');
+}
+console.log('My program does not stop');
