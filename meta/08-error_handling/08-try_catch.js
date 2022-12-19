65eff55943ff8cac33a678b66497601de0a6ef61
ralexrivero <ralexrivero@gmail.com>
2022-12-19 13:47:14 -0300

    try catch

--- meta/08-error_handling/08-try_catch.js
+++ meta/08-error_handling/08-try_catch.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
 
+
+try {
+    console.log(a + b);
+} catch(err) {
+    consol.log(err);
+    console.log('The error was saved y the error log');
+}
+
+console.log("m program does not stop");
