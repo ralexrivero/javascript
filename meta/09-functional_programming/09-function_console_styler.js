cd166c3ed47bcdf37fe8edfe3e61bacc2da1ecd3
ralexrivero <ralexrivero@gmail.com>
2022-12-22 08:59:14 -0300

    console format styler

---
+++ meta/09-functional_programming/09-function_console_styler.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+// Task 1: Build a function-based console log message generator
+
+function consoleStyler (color, background, fontSize, txt) {
+  let message = '%c' + txt;
+  let style = `color: ${color};`;
+  style += `background: ${background};`;
+  style += `font-size: ${fontSize};`;
+  console.log(`${message}`, style);
+}
+
+// Task 2: Build another console log message generator
+function celebrateStyler (reason) {
+  let fontStyle = 'color: tomato; font-size: 50px';
+  if (reason == 'birthday') {
+    console.log('%cHappy birthday', fontStyle);
+  } else if (reason == 'champions') {
+    console.log('%cCongrats con the title!', fontStyle);
+  } else {
+    console.log(message, style);
+  }
+}
+
+// Task 3: Run both the consoleStyler and the celebrateStyler functions
+
+let color = '#1d5c63';
+let background = '#ede6db';
+let fontSize = '40px';
+let txt = 'Congrats!';
+let reason = 'birthday';
+
+consoleStyler(color, background, fontSize, txt);
+celebrateStyler(reason);
+
+// Task 4: Insert a congratulatory and custom message
+function styleAndCelebrate () {
+  consoleStyler(color, background, fontSize, txt);
+  celebrateStyler(reason);
+}
+
+// Call styleAndCelebrate
+color = '#ef7c8e';
+background = '#fae8e0';
+fontSize = '30px';
+txt = 'You made it';
+reason = 'champions';
+styleAndCelebrate(color, background, fontSize, txt, reason);
