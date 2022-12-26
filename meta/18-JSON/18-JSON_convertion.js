59cf0ab5ec20b43fe4affb71db677981f7bbf408
ralexrivero <ralexrivero@gmail.com>
2022-12-26 03:12:03 -0300

    use github url to fetch json data

--- meta/18-JSON/18-JSON_convertion.js
+++ meta/18-JSON/18-JSON_convertion.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 async function populate() {
-  const requestURL = './data.json';
+  const requestURL = 'https://raw.githubusercontent.com/ralexrivero/javascript/main/meta/18-JSON/data.json';
   const request = new Request(requestURL);
 
   const response = await fetch(request);
