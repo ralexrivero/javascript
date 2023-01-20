ba4ab0c6baaf72a1f82d7afb2ac6a585f962b4f5
ralexrivero <ralexrivero@gmail.com>
2023-01-20 15:16:43 -0300

    types of vars

---
+++ universidad/00-intro/04-var_let_const.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/**
+ * without keyword
+ * literals
+ * var
+ * let
+ * const
+ */
+
+// without keywords
+x = 0;
+console.log(`${x} is typeof: ${typeof x}`);
+
+// literals
+1;
+10;
+'Charles';
+
+// var
+// avoid using
+
+// let
+// can be reassigned and assigned later
+
+// const
+// can't be modified and must be declared on initialization
+
+// variable names
+
+const camelCase = 'This is a cammel case';
+console.log(camelCase);
