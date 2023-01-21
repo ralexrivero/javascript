c77f57cef9c81685f50de9340c5d275a64c7ba9d
ralexrivero <ralexrivero@gmail.com>
2023-01-21 01:40:32 -0300

    operadores

---
+++ universidad/01-operadores/01-aritmeticos.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+/**
+ * operators
+ */
+
+// asignment
+
+const x = 1;
+const y = 2;
+
+// adition
+const z = x + y;
+console.log(z);
+
+// subtraction
+
+const a = 15 - 7;
+console.log(a);
+
+// multiplication
+const b = z * y;
+console.log(b);
+
+// exponentiation
+const c = 8 ** 2;
+console.log(c);
+
+// division
+const d = 96 / 4;
+console.log(d);
+
+// modulus
+
+const e = 97 % 3;
+console.log(e);
+
+// increment
+
+let f = 100;
+f++;
+console.log(f);
+
+// decrement
+let g = 99;
+g--;
+console.log(g);
